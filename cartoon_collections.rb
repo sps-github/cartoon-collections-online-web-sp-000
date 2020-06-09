87ab23f2f5325da081e0d5a784956c6bfb6a8a02
Sarah Peters <sarahpieters15@gmail.com>
2020-06-09 15:29:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map! { |name| name.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
