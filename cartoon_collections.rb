b05d0ebaac94b398cb53e1aac23302bf67648de8
Sarah Peters <sarahpieters15@gmail.com>
2020-06-09 15:53:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,10 +9,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
+  calls.any = false
   if calls.any? { |words| words.length < 4}
     return true
-  else
-    return false
   end
 end
 
