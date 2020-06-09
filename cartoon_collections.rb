70b9920a288ad98746c1229270396d2f144fd5cd
Sarah Peters <sarahpieters15@gmail.com>
2020-06-09 15:38:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,11 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? { |words| words.length < 4}
+  if calls.any? { |words| words.length < 4}
+    return true
+  else
+    return false
+  end
 end
 
 def find_the_cheese# code an argument here
